3e70b4bd2d7334c7fc8a3f85c4a1ab3d7908c859
yuk <dbr2314@gmail.com>
2025-05-14 20:09:43 +0900

    [feat/#??] : 홈 화면 구현, 카카오 지도 적용

--- podmate/src/App.tsx
+++ podmate/src/App.tsx
@@ -1,21 +1,44 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/LoginPage';
-import OAuthRedirect from './components/OAuthRedirect';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "@emotion/styled";
+import LoginPage from "./pages/LoginPage";
+import OAuthRedirect from "./components/OAuthRedirect";
+import Home from "./pages/Home";
+import NavigationBar from "./components/NavigationBar";
 
-function App() {
-    return (
-        <div className="viewport">
-            <div className="mobile-frame">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/oauth/redirect " element={<OAuthRedirect />} />
-                        <Route path="/" element={<LoginPage />} />
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        </div>
-    );
-}
+const BackGround = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: #cccccc; /* 회색 */
+  height: 100%;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const MobileFrame = styled.div`
+  width: 390px; /* iPhone 12 크기 예시 */
+  height: 100%;
+  background-color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+`;
+
+const App = () => {
+  return (
+    <Router>
+      <BackGround>
+        <MobileFrame>
+          <Routes>
+            <Route path="/oauth/redirect" element={<OAuthRedirect />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<div>등록 페이지</div>} />
+            <Route path="/mypage" element={<div>마이페이지</div>} />
+          </Routes>
+          <NavigationBar />
+        </MobileFrame>
+      </BackGround>
+    </Router>
+  );
+};
 
 export default App;
