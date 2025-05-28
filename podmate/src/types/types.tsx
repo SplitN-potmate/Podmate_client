77c01c5de1c79ab3c13c1af163ea1e86d9ee3cab
yuk <dbr2314@gmail.com>
2025-05-28 21:16:01 +0900

    [update] : 맵에서 팟 정보 보기

--- podmate/src/types/types.tsx
+++ podmate/src/types/types.tsx
@@ -1,37 +1,66 @@
 export interface PodProps {
-    podId: number;
-    podName: string;
-    podType: string;
-    itemUrl: string;
-    currentAmount: number;
-    goalAmount: number;
-    jjim: boolean;
+  podId: number;
+  podName: string;
+  podType: string;
+  platform: string;
+  itemUrl: string;
+  currentAmount: number;
+  goalAmount: number;
+  jjim: boolean;
+  latitude: number;
+  longitude: number;
+  road_address?: string;
+  roadAddress?: string;
+  addressId?: number;
+}
+export interface MapBounds {
+  swLat: number;
+  swLng: number;
+  neLat: number;
+  neLng: number;
+}
+
+interface AddressInfo {
+  main: string;
+  sub: string;
+}
+
+export interface MapState {
+  map: any | null;
+  isLoading: boolean;
+  isMapLoading: boolean;
+  isContainerReady: boolean;
+  pods: PodProps[];
+  currentAddress: AddressInfo;
+  bounds: MapBounds | null;
+  markers: any[];
+  selectedPod: PodProps | null;
 }
 
 export interface HeaderButtonProps {
-    type1: string;
-    type2: string;
-    activeType: string;
+  type1: string;
+  type2: string;
+  activeType: string;
 }
 
 export type postPodMinOrderProps = {
-    podName: string;
-    platform: string;
-    addressId: number;
-    endDate: Date | null;
-    totalAmount: number;
-    description: string | null;
+  podName: string;
+  platform: string;
+  addressId: number;
+  endDate: Date | null;
+  totalAmount: number;
+  description: string | null;
 };
 
 export interface postPodAddressProps {
-    roadAddress: string;
-    latitude: number;
-    longitude: number;
+  roadAddress: string;
+  latitude: number;
+  longitude: number;
 }
 
 export type placeProps = {
-    address: string;
-    detailedAddress: number | string;
-    latitude: number;
-    longitude: number;
+  address: string;
+  detailedAddress: number | string;
+  latitude: number;
+  longitude: number;
 };
