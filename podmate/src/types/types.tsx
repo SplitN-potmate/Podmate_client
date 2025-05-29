64364b3f5c4f1e858c8b44d4954b3cbe6f7b3a9e
Kangyeeun0 <kangyeeun55@gmail.com>
2025-05-29 22:35:28 +0900

    feat: 장바구니/장바구니 내 상품 삭제&추가

--- podmate/src/types/types.tsx
+++ podmate/src/types/types.tsx
@@ -1,57 +1,56 @@
 export interface PodProps {
-
-  podId: number;
-  podName: string;
-  podType: string;
-  platform: string;
-  itemUrl: string;
-  currentAmount: number;
-  goalAmount: number;
-  jjim: boolean;
-  latitude: number;
-  longitude: number;
-  road_address?: string;
-  roadAddress?: string;
-  addressId?: number;
+    podId: number;
+    podName: string;
+    podType: string;
+    platform: string;
+    itemUrl: string;
+    currentAmount: number;
+    goalAmount: number;
+    jjim: boolean;
+    latitude?: number;
+    longitude?: number;
+    road_address?: string | null;
+    roadAddress?: string | null;
+    addressId?: number | null;
+    podStatus: string;
 }
 export interface MapBounds {
-  swLat: number;
-  swLng: number;
-  neLat: number;
-  neLng: number;
+    swLat: number;
+    swLng: number;
+    neLat: number;
+    neLng: number;
 }
 
 interface AddressInfo {
-  main: string;
-  sub: string;
+    main: string;
+    sub: string;
 }
 
 export interface MapState {
-  map: any | null;
-  isLoading: boolean;
-  isMapLoading: boolean;
-  isContainerReady: boolean;
-  pods: PodProps[];
-  currentAddress: AddressInfo;
-  bounds: MapBounds | null;
-  markers: any[];
-  selectedPod: PodProps | null;
-
+    map: any | null;
+    isLoading: boolean;
+    isMapLoading: boolean;
+    isContainerReady: boolean;
+    pods: PodProps[];
+    currentAddress: AddressInfo;
+    bounds: MapBounds | null;
+    markers: any[];
+    selectedPod: PodProps | null;
 }
 
 export interface HeaderButtonProps {
-  type1: string;
-  type2: string;
-  activeType: string;
+    type1: string;
+    type2: string;
+    activeType: string;
 }
 
 export type postPodMinOrderProps = {
-  podName: string;
-  platform: string;
-  addressId: number;
-  endDate: Date | null;
-  totalAmount: number;
-  description: string | null;
+    podName: string;
+    platform: string;
+    addressId: number;
+    endDate: Date | null;
+    totalAmount: number;
+    description: string | null;
 };
 
 export type postPodGroupBuyProps = {
@@ -67,14 +66,30 @@ export type postPodGroupBuyProps = {
 };
 
 export interface postPodAddressProps {
-  roadAddress: string;
-  latitude: number;
-  longitude: number;
+    roadAddress: string;
+    latitude: number;
+    longitude: number;
 }
 
 export type placeProps = {
-  address: string;
-  detailedAddress: number | string;
-  latitude: number;
-  longitude: number;
+    address: string;
+    detailedAddress: number | string;
+    latitude: number;
+    longitude: number;
+};
+
+export interface postCartItemsProps {
+    platformInfoId: number;
+    itemList: CartItem[];
+}
+
+export type CartItem = {
+    quantity: number;
+    itemUrl: string;
+    optionText: string;
+};
+
+export type StoreItem = CartItem & {
+    itemName: string;
+    itemId: number;
 };
